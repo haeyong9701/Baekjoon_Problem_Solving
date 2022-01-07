a828c5ef0d246c717e54c18f9ffb318f90f83dd5
haeyong9701 <haeyong9701@gmail.com>
2022-01-08 00:11:14 +0900

    edit 2751번 수정렬하기 2

--- 2751-수정렬하기2.js
+++ 2751-수정렬하기2.js
@@ -1,17 +1,12 @@
 const fs = require("fs");
 // TODO: 제출 시 절대경로로 변환 필수 ("/dev/stdin")
-// const input = fs.readFileSync("test.txt").toString().split("\n");
-const input = require("fs").readFileSync("test.txt", "utf8");
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
 
 solution(input);
 
 function solution(input) {
-  let [firstLine, ...data] = input
-    .trim()
-    .split("\n")
-    .map((v) => Number(v));
-
-  const result = data.sort((a, b) => a - b);
-
+  input.shift();
+  const NumberArr = input.map((x) => Number(x));
+  const result = NumberArr.sort((a, b) => a - b);
   console.log(result.join("\n"));
 }
