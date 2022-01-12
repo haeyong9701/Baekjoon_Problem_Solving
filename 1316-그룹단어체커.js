9b74251801b87ea686cc7fcc56a8c6e223cc1ad2
haeyong9701 <haeyong9701@gmail.com>
2022-01-12 23:57:31 +0900

    Add 1316번 그룹단어체크 풀이중

---
+++ 1316-그룹단어체커.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+// TODO: 제출 시 절대경로로 변환 필수 ("/dev/stdin")
+const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+
+solution(input);
+
+function solution(input) {
+  input.shift();
+  let temp = [];
+  let count = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    temp.push(input[i][0]);
+    for (let j = 1; j < input[i].length; j++) {
+      if (input[i][j] !== input[i][j - 1]) {
+        temp.push(input[i][j]);
+        console.log(temp);
+      }
+    }
+    temp = [];
+
+    console.log(temp.length);
+    // console.log([...new Set(input[0].split(""))].length);
+    if ([...new Set(input[0].split(""))].length === temp.length) {
+      console.log(temp);
+      count++;
+    }
+  }
+  console.log(count);
+}
